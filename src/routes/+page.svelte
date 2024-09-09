ed406841d6ddf24b06f31b378fb1e157d07a9534
Tee Ming <chewteeming01@gmail.com>
2024-09-09 22:02:37 +0800

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const { form }: {
+    form: {
+      example: string;
+    }
+  } = $derived($page.form);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
